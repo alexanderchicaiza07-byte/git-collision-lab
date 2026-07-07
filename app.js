6e2d7efef8dd9dab10d38a9d5f7a800f7121d9bb
Alexander Chicaiza <alexanderchicaiza07@gmail.com>
2026-07-06 19:55:29 -0500

    feat: implementar descuento del 15 por ciento

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function CalcularPrecio(){
 
-    precio = document.getElementById("precio").value;
-    
+    precio = document.getElementById("precio");
+    cantidad = document.getElementById("cantidad").value;
+
+    return precios * 0.85 //Descuento estudiante B 
 }
